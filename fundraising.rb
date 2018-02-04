029bb864f3bac32d0079c51e69c748833e1bccf2
Julie Anne Wear <jag68@icloud.com>
2018-02-04 14:31:11 -0800

    Initial commit

---
+++ fundraising.rb
@@ -0,0 +1,9 @@
+project1 = 'Cat Surgery'
+project2 = 'Dog Surgery'
+project3 = 'Animal Shelter and Rescue'
+
+fund1 = 1000
+
+puts "Project #{project1} has $#{fund1} in funding."
+
+puts "Projects: \n\t#{project1}\n\t#{project2}\n\t#{project3}"
