15ce53293fc22a2f69f5274f8cac5a996e03f8fd
Julie Anne Wear <jag68@icloud.com>
2018-02-04 20:38:26 -0800

    Added more projects and funding

--- fundraising.rb
+++ fundraising.rb
@@ -1,9 +1,18 @@
 project1 = 'Cat Surgery'
 project2 = 'Dog Surgery'
 project3 = 'Animal Shelter and Rescue'
+project4 = 'Vet Treatment Cost'
 
-fund1 = 1000
+funding1 = 1000
+funding2 = 5500
+funding3 = 2800
+funding4 = 4000
 
-puts "Project #{project1} has $#{fund1} in funding."
+current_time = Time.new
 
-puts "Projects: \n\t#{project1}\n\t#{project2}\n\t#{project3}"
+puts "Project #{project1.capitalize} has $#{funding1} in funding."
+puts "Project #{project2.upcase} has $#{funding2} in funding."
+puts "Project #{project3.capitalize} has $#{funding3} in funding.".center(50, '*')
+puts "Project #{project4.capitalize.ljust(40, '.')} has $#{funding4} in funding."
+
+puts "Funding started on #{current_time.strftime("%A %m/%d/%Y at %I:%m%p")}."
