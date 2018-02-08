1c903b378596d88775296508a3141e7937f64088
Julie Anne Wear <jag68@icloud.com>
2018-02-07 18:56:09 -0800

    Added attributes

--- fundraising.rb
+++ fundraising.rb
@@ -5,10 +5,17 @@ class String
 end
 
 class Project
+  attr_accessor :name
+  attr_reader :current, :target
+
   def initialize(name, current=0, target)
     @name = name.titlecase
     @current = current
     @target = target
+  end
+
+  def name=(new_name)
+    @name = new_name.titlecase
   end
 
   def add
@@ -21,16 +28,27 @@ class Project
     "Project #{@name} lost some funds!"
   end
 
+  def need
+    @target - @current
+  end
+
   def to_s
-    "Project #{@name} has $#{@current} in funding towards a goal of $#{@target}."
+    "Project #{@name} has $#{@current} in funding towards a goal of $#{@target}.\nThe project still needs $#{need} to reach the goal."
   end
 end
 
-proj1 = Project.new("cat surgery", 3000)
+proj1 = Project.new("cat surgery", 500, 3000)
+# puts proj1
+# puts proj1.add
+# puts proj1
+# puts proj1.add
+# puts proj1
+# puts proj1.subtract
+# puts proj1
 puts proj1
-puts proj1.add
-puts proj1
-puts proj1.add
-puts proj1
-puts proj1.subtract
+proj1.name = "dog surgery"
+puts proj1.name
+puts proj1.current
+puts proj1.target
+puts proj1.need
 puts proj1
