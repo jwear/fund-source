2af481628584dc22282236261958cbee1ba79c7e
Julie Anne Wear <jag68@icloud.com>
2018-02-10 14:49:15 -0800

    Created Category class and separated source files

--- fundraising.rb
+++ fundraising.rb
@@ -1,52 +1,11 @@
-class String
-  def titlecase
-    "#{self.split.each{ |word| word.capitalize!}.join(' ')}"
-  end
-end
-
-class Project
-  attr_accessor :name
-  attr_reader :current, :target
-
-  def initialize(name, current=0, target)
-    @name = name.titlecase
-    @current = current
-    @target = target
-  end
-
-  def name=(new_name)
-    @name = new_name.titlecase
-  end
-
-  def add
-    @current += 50
-    "Project #{@name} got more funds!"
-  end
-
-  def subtract
-    @current -= 10
-    "Project #{@name} lost some funds!"
-  end
-
-  def need
-    @target - @current
-  end
-
-  def to_s
-    "Project #{@name} has $#{@current} in funding towards a goal of $#{@target}.\nThe project still needs $#{need} to reach the goal."
-  end
-end
+require_relative 'category'
 
 project1 = Project.new("cat surgery", 500, 3000)
 project2 = Project.new("vet treatment cost", 150, 2500)
 project3 = Project.new("dog surgery", 1000, 5000)
 
-projects = [project1, project2, project3]
-
-projects.each do |project|
-  puts project.add
-  puts project.add
-  puts project.subtract
-  puts project.add
-  puts project
-end
+animals = Category.new("Animals")
+animals.add_project(project1)
+animals.add_project(project2)
+animals.add_project(project3)
+animals.request_funding
